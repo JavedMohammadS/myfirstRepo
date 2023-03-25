e8e410d1173abdc85328c3384699f7e5fda1b9bc
Javed M Sahibzai <javed.sahibzai@tekschool.us>
2023-03-25 02:37:07 -0400

    old deleted

--- src/test/UnitTest.java
+++ src/test/UnitTest.java
@@ -4,11 +4,15 @@ public class UnitTest {
 	public static void main(String[] args) {
 		
 		
-		int a = 110;
-		int b = 10;
+		int a = 100;
+		int b = 15;
 		int c = a+b;
 		System.out.println(c);
+		System.out.println(testone(10,5));
+		
 		
 	}
-
+	public static int testone(int a, int b) {
+		return a-b;
+	}
 }
